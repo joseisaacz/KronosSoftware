7788aa32311b93c137a76bd5ff7bd744b0eb7889
joseisaacz <39786721+joseisaacz@users.noreply.github.com>
2020-02-16 21:30:15 -0600

    some changes

---
+++ src/main/java/com/kronos/controller/AdministrationController.java
@@ -0,0 +1,28 @@
+package com.kronos.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kronos.service.TempUserService;
+import com.kronos.service.UserService;
+import com.kronos.model.User;
+
+@Controller
+@RequestMapping(value= "/administration")
+public class AdministrationController {
+	
+	@Autowired
+	private TempUserService tmp;
+	@Autowired 
+	private UserService user;
+	
+	@GetMapping("/addUser")
+	public String addUser(User user, Model model) {
+		model.addAttribute("user", user);
+		return "administration/userForm"; 
+	}
+	
+}
--- src/main/java/com/kronos/schedule/Scheduler.java
+++ src/main/java/com/kronos/schedule/Scheduler.java
@@ -1,8 +1,15 @@
 package com.kronos.schedule;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.*;
+
+import com.kronos.model.Accord;
+import com.kronos.service.AccordService;
 
 @Configuration
 @EnableScheduling
@@ -11,10 +18,22 @@ public class Scheduler {
 
 	@Autowired 
 	private EmailServiceImpl email;
+	@Autowired
+	private AccordService acc;
 	
-	@Scheduled(cron="00 15 12 * * SAT")
-	public void weeklyNotification() {
-		email.sendSimpleMail("jjestradav@gmail.com", "Prueba", "Que siente bb?");
-		System.out.println("this is a fucking test");
+	@Scheduled(cron="00 36 16 * * SAT")
+	public void weeklyNotification() throws Exception {
+		Calendar limit= Calendar.getInstance();
+	    limit.set(Calendar.DAY_OF_MONTH,limit.get(Calendar.DAY_OF_MONTH)-8);
+	    Date lmt=limit.getTime();
+	    Date today = Calendar.getInstance().getTime();
+	    List<Accord> list = acc.emailInfo(today,lmt);
+	    String message=" ";
+	    for(Accord a : list){
+	     message= message +"\n"+ a.toString();
+	    }
+	    String message1= "Los acuerdos notificados esta semana son los siguientes \n"+message;
+		email.sendSimpleMail("jjestradav@gmail.com", "Acuerdos de la semana", message1);
+		System.out.println(message1);
 	}
 }
