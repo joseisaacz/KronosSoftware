c73a307e8b00ad988695312056be296fcb79b4f1
Jonathan <jjestradav@github.com>
2020-02-27 14:53:31 -0600

    Some changes

--- src/main/java/com/kronos/controller/AdministrationController.java
+++ src/main/java/com/kronos/controller/AdministrationController.java
@@ -26,7 +26,6 @@ import com.kronos.service.UserRoleService;
 import com.kronos.service.UserService;
 import com.kronos.model.TempUser;
 import com.kronos.model.User;
-import com.kronos.model.UserRole;
 import com.kronos.model.Department;
 import com.kronos.model.Role;
 
